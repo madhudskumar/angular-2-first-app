fe5b2de394c76364a00a15697285fe8de75b7992
Madhu Kumar D S <madhudskumar@live.in>
2017-01-10 22:09:00 +0530

    lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Root componet</h1>
-    <app-databinding></app-databinding>
+    <app-lifecycle *ngIf="!delete" [bindable]="bindValue">
+      {{test}}
+    </app-lifecycle>
+    <br>
+    <button (click)="delete = true">click to destroy</button>
+    <button (click)="test = 'changed value'">change-content</button>
+    <button (click)="bindValue = 2000">change-binding</button>
   `,
 })
-export class AppComponent {
-
+export class AppComponent{
+  delete = false;
+  test = "starting value"
+  bindValue = 1000;
 }
+
--- src/app/lifecycle.component.ts
+++ src/app/lifecycle.component.ts
@@ -1,19 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked,
+  OnDestroy, Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   template: `
-    <p>
-      lifecycle Works!
-    </p>
-  `,
-  styles: []
+    <ng-content></ng-content>
+    <hr>
+    <p>{{bindable}}</p>
+  `
 })
-export class LifecycleComponent implements OnInit {
+export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy{
+  @Input() bindable = 1000;
 
-  constructor() { }
+  constructor() {
+
+  }
+
+  ngOnChanges() {
+    this.log('ng-on-changes');
+  }
 
   ngOnInit() {
+    this.log('ng-on-init');
+  }
+
+  ngDoCheck(){
+    this.log('ng-fo-check');
+  }
+
+  ngAfterContentInit(){
+    this.log('ng-after-content-init');
+  }
+
+  ngAfterContentChecked(){
+    this.log('ng-after-content-checked');
+  }
+
+  ngAfterViewChecked(){
+    this.log('ng-after-view-checked');
+  }
+
+  ngAfterViewInit(){
+    this.log('ng-after-view-init');
+  }
+
+  ngOnDestroy(){
+    this.log('ng-on-destroy');
+  }
+
+  private log(hook:string){
+    console.log(hook);
   }
 
 }
