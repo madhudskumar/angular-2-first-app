dd17d478cb4cb00fff3081d37b711c39cea0fe07
Madhu Kumar D S <madhudskumar@live.in>
2017-01-10 20:50:26 +0530

    event bindind

--- src/app/databinding/event-binding.component.ts
+++ src/app/databinding/event-binding.component.ts
@@ -1,19 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-event-binding',
   template: `
-    <p>
-      event-binding Works!
-    </p>
-  `,
-  styles: []
+    <button (click)="onClicked()">click me</button>
+  `
 })
-export class EventBindingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class EventBindingComponent {
+  onClicked(){
+    alert("it worked");
   }
-
 }
