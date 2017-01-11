2d9fe7a455e98340dcff67efdc46231112401ce2
Madhu Kumar D S <madhudskumar@live.in>
2017-01-11 22:08:27 +0530

    added custom hostBinding

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-
-  constructor() { }
+  private backgroundColor = 'white';
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = "green";
+  };
+  @HostBinding('style.backgroundColor') get setColor() {
+    return this.backgroundColor;
+  }
+  constructor(private elementRef: ElementRef, private rendrer: Renderer) {
+    // this.elementRef.nativeElement.style.backgroundColor = 'green';
+    // this.rendrer.setElementStyle(this.elementRef.nativeElement, "background-color", "green");
+  }
 
 }
