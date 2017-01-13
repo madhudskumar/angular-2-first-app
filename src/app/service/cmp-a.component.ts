3a87e56ba24cf88972b78ecfdb218ce2e745623c
Madhu Kumar D S <madhudskumar@live.in>
2017-01-13 06:45:19 +0530

    added log services

--- src/app/service/cmp-a.component.ts
+++ src/app/service/cmp-a.component.ts
@@ -3,17 +3,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cmp-a',
   template: `
-    <p>
-      cmp-a Works!
-    </p>
+    <div>
+      <input type="text" #input>
+      <button (click)="onLog(input.value)">Log</button>
+      <button (click)="onStore(input.value)">Store</button>
+      <button (click)="onSend(input.value)">Send</button>
+    </div>
+    <hr>
+    <div>
+        <button (click)="onGet()">Refresh Storage</button>
+        <h3>Storage</h3>
+        <ul>
+            <li *ngFor="let item of items">{{item}}</li>
+        </ul>
+        <h3>Received Value</h3>
+        <p>{{value}}</p>
+    </div>
   `,
   styles: []
 })
 export class CmpAComponent implements OnInit {
+  value = '';
+  items: string[] = [];
 
-  constructor() { }
+  onLog(value: string) {
 
-  ngOnInit() {
   }
 
+  onStore(value: string) {
+
+  }
+
+  onGet() {
+
+  }
+
+  onSend(value: string) {
+
+  }
+
+  ngOnInit(){
+
+  }
 }
--- src/app/service/cmp-b.component.ts
+++ src/app/service/cmp-b.component.ts
@@ -3,17 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cmp-b',
   template: `
-    <p>
-      cmp-b Works!
-    </p>
+     <div>
+      <input type="text" #input>
+      <button (click)="onLog(input.value)">Log</button>
+      <button (click)="onStore(input.value)">Store</button>
+    </div>
+    <hr>
+    <div>
+        <button (click)="onGet()">Refresh Storage</button>
+        <h3>Storage</h3>
+        <ul>
+            <li *ngFor="let item of items">{{item}}</li>
+        </ul>
+        <h3>Received Value</h3>
+        <p>{{value}}</p>
+    </div>
   `,
   styles: []
 })
 export class CmpBComponent implements OnInit {
+  value = '';
+  items: string[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  onLog(value: string) {
   }
 
+  onStore(value: string) {
+  }
+
+  onGet() {
+  }
+
+  ngOnInit() {
+
+  }
 }
