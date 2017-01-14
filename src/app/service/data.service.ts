6c8ccf716b736d48a79ab380ef217ceb77fc686c
Madhu Kumar D S <madhudskumar@live.in>
2017-01-14 19:56:57 +0530

    updated dta service

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,8 +1,14 @@
+import {LogService} from "./log.service";
 export class DataService {
   private data:string[] = [];
 
+  constructor(private logService:LogService){
+
+  }
+
   addData(ip:string){
     this.data.push(ip)
+    this.logService.writeToLog(ip);
   }
 
   getData(){
