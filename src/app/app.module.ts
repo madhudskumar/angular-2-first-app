fcde87573d955d3cb99db748dff28eea38c46ffe
Madhu Kumar D S <madhudskumar@live.in>
2017-01-18 14:46:40 +0530

    added form control datadriven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -36,6 +36,7 @@ import { DataDrivenComponent } from './forms/data-driven/data-driven.component';
     DataDrivenComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
     FormsModule,
     HttpModule
--- src/app/forms/data-driven/data-driven.component.ts
+++ src/app/forms/data-driven/data-driven.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-data-driven',
   templateUrl: './data-driven.component.html',
-  styleUrls: ['./data-driven.component.css']
+  styleUrls: ['./data-driven.component.css'],
 })
 export class DataDrivenComponent implements OnInit {
+  myForm:FormGroup;
 
-  constructor() { }
+  constructor(  ) {
+    this.myForm = new FormGroup({
+      'username': new FormControl('Max',<any>Validators.required),
+      'email': new FormControl('', [<any>Validators.required, <any>Validators.pattern("/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;")] ),
+      'password': new FormControl('', <any>Validators.required),
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.myForm);
   }
 
 }
