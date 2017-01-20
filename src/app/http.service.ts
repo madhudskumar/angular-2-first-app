adafb552d91d09f872525c7955bc711dbaafdc06
Madhu Kumar D S <madhudskumar@live.in>
2017-01-20 14:35:31 +0530

    f:http-service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@angular/core';
+import {Http, Response, Headers} from "@angular/http";
+import 'rxjs/Rx'
+import {Observable} from "rxjs";
 
 @Injectable()
 export class HttpService {
 
-  constructor() { }
+  constructor(private http:Http) { }
 
+  getData(){
+    return this.http
+      .get("https://angular2-cource-3badd.firebaseio.com/title.json")
+      .map((response:Response) => response.json())
+  }
+
+  sendData(user:any){
+    const body = JSON.stringify(user);
+    const headers = new Headers({
+      'Content-Type':'application/json'
+    });
+    return this.http
+      .post(
+        'https://angular2-cource-3badd.firebaseio.com/data.json',
+        body,
+        {
+          headers: headers
+        }
+      )
+      .map((data:Response) => data.json())
+      .catch(this.handleError);
+  }
+
+  getOwnData(){
+    return this.http
+      .get("https://angular2-cource-3badd.firebaseio.com/data.json")
+      .map((response:Response) => response.json())
+  }
+
+  private handleError(error:any){
+    console.log(error);
+    return Observable.throw(error);
+  }
 }
--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {HttpService} from "../http.service";
+import {Subscription} from "rxjs";
+// import {Response} from "@angular/http";
 
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
-  styleUrls: ['./http.component.css']
+  styleUrls: ['./http.component.css'],
+  providers:[HttpService]
 })
-export class HttpComponent implements OnInit {
+export class HttpComponent implements OnInit, OnDestroy {
+  private title;
+  private data:string[] = [];
+  private titleSubscriber:Subscription;
 
-  constructor() { }
 
-  ngOnInit() {
+  asyncStr = this.httpService.getData();
+  constructor(private httpService:HttpService) { }
+
+  onGetData(){
+    this.httpService.getOwnData()
+      .subscribe(
+        data => {
+          const myArray = [];
+          for(let key in data){
+            myArray.push(data[key]);
+          }
+          this.data = myArray
+        }
+      )
   }
 
+  ngOnInit() {
+    this.titleSubscriber = this.httpService.getData()
+      .subscribe(
+        (data:any) =>{
+          console.log(data);
+          this.title = data;
+        }
+      )
+  }
+
+  ngOnDestroy(){
+    this.titleSubscriber.unsubscribe();
+  }
+
+  onSubmit(username:string, email:string){
+    this.httpService
+      .sendData({
+        username:username,
+        email:email
+      })
+      .subscribe(
+        (data:any) => console.log(data)
+      )
+  }
 }
