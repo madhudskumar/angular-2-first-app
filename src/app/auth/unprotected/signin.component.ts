a94f26688ebc5d89c9bfcdd237cacb6a76e1a205
Madhu Kumar D S <madhudskumar@live.in>
2017-01-21 07:21:25 +0530

    i:signup signin

--- src/app/auth/unprotected/signin.component.ts
+++ src/app/auth/unprotected/signin.component.ts
@@ -1,19 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
   template: `
-    <p>
-      signin Works!
-    </p>
+    <h3>Please sign up to use all features</h3>
+        <form [formGroup]="myForm" (ngSubmit)="onSignin()">
+            <div class="input-group">
+                <label for="email">E-Mail</label>
+                <input formControlName="email" type="email" id="email">
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input formControlName="password" type="password" id="password">
+            </div>
+            <button type="submit" [disabled]="!myForm.valid">Sign In</button>
+        </form>
   `,
   styles: []
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+   myForm: FormGroup;
+    error = false;
+    errorMessage = '';
 
-  ngOnInit() {
-  }
+    constructor(private fb: FormBuilder) {}
+
+    onSignin() {
+
+    }
+
+    ngOnInit():any {
+        this.myForm = this.fb.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+        });
+    }
 
 }
